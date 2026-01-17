1a1f770bcf1e25f573b780144d81bce601881c2a
Vinoth <7010291496vinotht@gmail.com>
2026-01-17 08:25:26 +0530

    merge

--- src/pages/SuperDeals.jsx
+++ src/pages/SuperDeals.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import API from "../api/api";
 import { Zap, Clock, Search, Trash2, Plus, ArrowRight, Tag, Percent, Calculator } from 'lucide-react'
 
-const API_URL = "product/Superdealdata";
+const API_URL = "product/superdealsdata";
 
 export default function SuperDeals() {
 
